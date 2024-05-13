e4fc464ecdd7df8adf3349c9a8b5fceee1b1e260
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2024-05-14 00:39:51 +0800

    0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="hakuir",
     packages=["hakuir"],
-    version="0.0.1",
+    version="0.0.2",
     install_requires=[
         "torch",
         "torchvision",
